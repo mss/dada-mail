312e86ce6d6b48fc5fe5ddd117adf90e46950f30
Malte S. Stretz <mss@apache.org>
2010-09-27 01:15:14 +0200

    Add locking to FileHandle.

--- dada/DADA/FileHandle.pm
+++ dada/DADA/FileHandle.pm
@@ -5,6 +5,10 @@ use strict;
 use IO::File;
 our @ISA = qw(IO::File);
 
+use Errno qw(
+	EEXIST
+);
+
 use Fcntl qw(
 	:flock
 	LOCK_EX
@@ -36,22 +40,35 @@ sub new
 	croak("missing file") unless $file;
 	
 	my $self = $class->SUPER::new();
-	${*self}{dada_file} = $file;
+	${*$self}{dada_file} = $file;
+	${*$self}{dada_lock} = undef;
 	bless $self, $class;
 	
 	return $self->open(@_) if (@_);
 	return $self;
 }
 
+sub DESTROY
+{
+	my($self) = @_;
+	$self->close();
+}
+
+
 sub open
 {
 	my ($self, $mode, $errhandler) = @_;
 	croak("missing mode") unless $mode;
 	
-	my($openmode, $binmode) = ($mode =~ /^([rwa][+]?)([buh])?$/);
+	# mode: r|r+|w|w+|a|a+ [b|u|h] [t]
+	#  * read, read/write, write, write/read, append, append/read
+	#  * binary, utf8, HTML_CHARSET
+	#  * flock timeout (ms), 0 disable flock
+	my($openmode, $binmode, $wait) = ($mode =~ /^([rwa][+]?)([buh])?(\d+)?$/);
 	croak("invalid mode $mode") unless $openmode;
+	$wait = FLOCK_WAIT unless defined $wait;
 	
-	my $file = ${*self}{dada_file};
+	my $file = ${*$self}{dada_file};
 	
 	sub _err {
 		$self->close();
@@ -80,33 +97,112 @@ sub open
 		}
 	}
 	
-	# Return early on Solaris since locking seems to be broken on 
-	# some systems (cf. description of lockf implementation
-	# in perldoc -f flock).  BTW:  This seems to be fixed at least
-	# on 'SunOS x 5.10 Generic_139556-08 i86pc i386 i86pc'
-	return $self if $^O eq 'solaris';
+	if ($wait) {
+		if ($openmode eq 'r') {
+			$self->flock(LOCK_SH, $wait) ||
+				return _err("failed to flock $file (sh): $!");
+		}
+		else {
+			$self->flock(LOCK_EX, $wait) ||
+				return _err("failed to flock $file (ex): $!");
+		}
+	}
 	
-	if ($openmode eq 'r') {
-		$self->flock(LOCK_SH) ||
-			return _err("failed to flock $file (sh): $!");
-	}
-	else {
-		$self->flock(LOCK_EX)  ||
-			return _err("failed to flock $file (ex): $!");
-	}
 	return $self;
 }
 
+sub close
+{
+	my($self) = @_;
+	
+	$self->unlock();
+	
+	return -1 unless $self->opened();
+	return $self->SUPER::close();
+}
+
+
+sub getname
+{
+	my($self) = @_;
+	return ${*$self}{dada_file};
+}
+
+
 sub flock
 {
-	my($self, $op) = @_;
-	my $stop = time() + FLOCK_WAIT / 1000;
-	do {
+	# Return early on Solaris since LOCK_EX locking seems to be broken 
+	# on some systems (cf. description of lockf implementation
+	# in perldoc -f flock).  BTW:  This seems to be fixed at least
+	# on 'SunOS x 5.10 Generic_139556-08 i86pc i386 i86pc'
+	return -1 if $^O eq 'solaris';
+
+	my($self, $op, $wait) = @_;
+	croak("missing op") unless defined $op;
+	$wait ||= 0;
+	
+	my $bang;
+	my $stop = time() + $wait / 1000;
+	my $done = 0;
+	while (!$done) {
+		$done = time() > $stop;
 		return 1 if flock($self, $op | LOCK_NB);
-		usleep(FLOCK_SLEEP);
-	} while (time() < $stop);
+		$bang = $! * 1;
+		usleep(FLOCK_SLEEP * 1000);
+	}
+	
+	$! = $bang;
+	
 	return 0;
 }
 
+
+sub lock
+{
+	my($self, $wait) = @_;
+	$wait ||= 0;
+	
+	my $file = ${*$self}{dada_file};
+	my $lock = ${*$self}{dada_lock};
+	return -1 if defined $lock;
+	
+	$lock = DADA::FileHandle->new($file . '.lock');
+	
+	my $stop = time() + $wait / 1000;
+	my $done = 0;
+	while (!$done) {
+		$done = time() > $stop;
+		$! = EEXIST;
+		next if -f $lock->getname();
+		if ($lock->open('a+1')) {
+			$lock->seek(0, SEEK_SET);
+			next if ($lock->getline());
+			$lock->truncate(0);
+			$lock->print("$$\n" . gmtime() . "\n");
+			${*$self}{dada_lock} = $lock;
+			return 1;
+		}
+		usleep(FLOCK_SLEEP * 1000);
+	}
+	
+	croak("failed to lock $file: $!");
+	return 0;
+}
+
+sub unlock
+{
+	my($self) = @_;
+	
+	my $file = ${*$self}{dada_file};
+	my $lock = ${*$self}{dada_lock};
+	return -1 unless defined $lock;
+	
+	$lock->close();
+	unlink($lock->getname()) ||
+		croak("failed to unlock $file: $!");
+	
+	${*$self}{dada_lock} = undef;
+	return 1;
+}
 
 1;
--- dada/t/DADA_FileHandle.t
+++ dada/t/DADA_FileHandle.t
@@ -2,7 +2,7 @@
 use strict;
 use warnings;
 
-use lib qw(./ ./DADA/perllib ../ ../DADA/perllib ../../ ../../DADA/perllib 
+use lib qw(./ ./DADA/perllib
 ./t
 );
 
@@ -24,40 +24,21 @@ sub f
   return "$d/$f"
 }
 
-sub t1
-{
-  my  $txt = shift;
-  my  $ref = shift;
-  my  $sub = shift;
-  our $err = undef;
-  my $ret = $ref->$sub(@_, sub { $err = shift; });
-  is($err, undef, $txt);
-  diag($ret);
-  return $ret;
-}
-
-sub t2
-{
-  my $txt = shift;
-  my $ref = shift;
-  my $sub = shift;
-  $! = undef;
-  my $ret = $ref->$sub(@_);
-  is(defined($!) ? $! ? $! : undef : undef, undef, $txt);
-  diag($ret);
-  return $ret;
-}
+sub t1;
+sub t2;
+sub t3;
+sub t4;
 
 my($f1, $f2);
 my($fh1, $fh2);
 my $ret;
 
+unlink(glob(f("*")));
 $f1 = f("test");
-unlink($f1);
 
 use constant DADAFH => 'DADA::FileHandle';
 
-$fh1 = t1("open file (w)", DADAFH, 'new', $f1, 'w');
+$fh1 = t1("open file (w0)", DADAFH, 'new', $f1, 'w0');
 $ret = t2("write file", $fh1, 'print', "foo\nbar\n");
 $ret = t2("close file", $fh1, 'close');
 $ret = t2("read file", $fh1, 'getline');
@@ -70,14 +51,11 @@ $ret = t2("close file", $fh1, 'close');
 
 $fh1 = t1("open file (w+)", DADAFH, 'new', $f1, 'w+');
 $ret = t2("read file", $fh1, 'getline');
-is($ret, undef, "read file: fail");
+is($ret, undef, "read file: empty");
 $ret = t2("write file", $fh1, 'print', "bla\n");
 $ret = t2("seek file", $fh1, 'seek', 0, SEEK_SET);
 is($fh1->getline(), "bla\n", "read file");
-
-$fh2 = DADAFH->new($f1, 'r');
-is($fh2, undef, "locked");
-
+$fh2 = t4("locked file", DADAFH, 'new', $f1, 'r');
 $ret = t2("close file", $fh1, 'close');
 
 $DADA::Config::HTML_CHARSET = 'utf16';
@@ -88,3 +66,68 @@ $fh1 = t1("open file (rh)", DADAFH, 'new', $f1, 'rh');
 $ret = $fh1->getline();
 is($ret, 'DADA', 'utf16');
 $ret = t2("close file", $fh1, 'close');
+
+$fh1 = t2("new file (1)", DADAFH, 'new', $f1);
+$ret = t3("lock file (1)", $fh1, 'lock', 100);
+$fh1 = t1("open file (1:r1)", $fh1, 'open', 'r1');
+$ret = t3("lock file (1)", $fh1, 'lock', 100);
+$fh2 = t1("open file (2:r1)", DADAFH, 'new', $f1, 'r1');
+$ret = t4("lock file (2)", $fh2, 'lock', 100);
+$ret = t2("unlock file (1)", $fh1, 'unlock');
+$ret = t3("lock file (2)", $fh2, 'lock', 100);
+$ret = t2("close file (2)", $fh2, 'close');
+$ret = t2("close file (1)", $fh1, 'close');
+$fh1 = t1("open file (1:r)", DADAFH, 'new', $f1, 'r');
+$ret = t2("lock file (1)", $fh1, 'lock', 1000);
+$ret = t2("close file (1)", $fh1, 'close');
+
+
+sub t1
+{
+  my  $txt = shift;
+  my  $ref = shift;
+  my  $sub = shift;
+  our $err = undef;
+  diag("--> next: $txt");
+  my $ret = $ref->$sub(@_, sub { $err = shift; });
+  diag((defined($ret) ? $ret : "undef") . ($ref ne DADAFH and $ref->can('getname') ? " (" . $ref->getname() . ")" : ""));
+  is($err, undef, $txt);
+  return $ret;
+}
+
+sub t2
+{
+  my $txt = shift;
+  my $ref = shift;
+  my $sub = shift;
+  $! = undef;
+  diag("--> next: $txt");
+  my $ret = $ref->$sub(@_);
+  diag((defined($ret) ? $ret : "undef") . ($ref ne DADAFH and $ref->can('getname') ? " (" . $ref->getname() . ")" : ""));
+  is($! ? $! : undef , undef, $txt);
+  return $ret;
+}
+
+sub t3
+{
+  my $txt = shift;
+  my $ref = shift;
+  my $sub = shift;
+  diag("--> next: $txt");
+  my $ret = eval { $ref->$sub(@_); };
+  diag((defined($ret) ? $ret : "undef") . ($ref ne DADAFH and $ref->can('getname') ? " (" . $ref->getname() . ")" : ""));
+  isnt($ret ? $ret : undef, undef, $txt);
+  return $ret;
+}
+
+sub t4
+{
+  my $txt = shift;
+  my $ref = shift;
+  my $sub = shift;
+  diag("--> next: $txt");
+  my $ret = eval { $ref->$sub(@_); };
+  diag((defined($ret) ? $ret : "undef") . ($ref ne DADAFH and $ref->can('getname') ? " (" . $ref->getname() . ")" : ""));
+  is($ret ? $ret : undef, undef, $txt);
+  return $ret;
+}
